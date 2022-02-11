771cde73bc8861d5c81f39b0e3c7c1b75c77404f
Dale <dale.vand@kakaoenterprise.com>
2022-02-11 16:09:12 +0900

    Refact: 코드 리뷰 반영(each문 이용하여 그리드 배치)

--- src/components/Keyboard.svelte
+++ src/components/Keyboard.svelte
@@ -1,40 +1,15 @@
-<script></script>
+<script>
+  import { keys } from '../utils/const';
+</script>
 
 <div class="container">
-  <div class="grid-row row-1">
-    <div class="grid-item" data-key="q">Q</div>
-    <div class="grid-item" data-key="w">W</div>
-    <div class="grid-item" data-key="e">E</div>
-    <div class="grid-item" data-key="r">R</div>
-    <div class="grid-item" data-key="t">T</div>
-    <div class="grid-item" data-key="y">Y</div>
-    <div class="grid-item" data-key="u">U</div>
-    <div class="grid-item" data-key="i">I</div>
-    <div class="grid-item" data-key="o">O</div>
-    <div class="grid-item" data-key="p">P</div>
-  </div>
-  <div class="grid-row row-2">
-    <div class="grid-item" data-key="a">A</div>
-    <div class="grid-item" data-key="s">S</div>
-    <div class="grid-item" data-key="d">D</div>
-    <div class="grid-item" data-key="f">F</div>
-    <div class="grid-item" data-key="g">G</div>
-    <div class="grid-item" data-key="h">H</div>
-    <div class="grid-item" data-key="j">J</div>
-    <div class="grid-item" data-key="k">K</div>
-    <div class="grid-item" data-key="l">L</div>
-  </div>
-  <div class="grid-row row-3">
-    <div class="grid-item" data-key="enter">ENTER</div>
-    <div class="grid-item" data-key="z">Z</div>
-    <div class="grid-item" data-key="x">X</div>
-    <div class="grid-item" data-key="c">C</div>
-    <div class="grid-item" data-key="v">V</div>
-    <div class="grid-item" data-key="b">B</div>
-    <div class="grid-item" data-key="n">N</div>
-    <div class="grid-item" data-key="m">M</div>
-    <div class="grid-item" data-key="back">BACK</div>
-  </div>
+  {#each keys as keysRow, i}
+    <div class="grid-row row-{i}">
+      {#each keysRow as key}
+        <div class="grid-item" data-key={key}>{key}</div>
+      {/each}
+    </div>
+  {/each}
 </div>
 
 <style>
@@ -60,13 +35,13 @@
     border-radius: 3px;
     color: var(--key-text-color);
   }
-  .row-1 {
+  .row-0 {
     grid-template-columns: repeat(10, 43px);
   }
-  .row-2 {
+  .row-1 {
     grid-template-columns: repeat(9, 43px);
   }
-  .row-3 {
+  .row-2 {
     grid-template-columns: 65px repeat(7, 43px) 65px;
   }
 </style>
