6236c55635a05660c465978c0fbf226c073cea7a
Dale <dale.vand@kakaoenterprise.com>
2022-02-13 14:23:26 +0900

    Refact: 매직넘버 변수명 수정

--- src/constants/keys.js
+++ src/constants/keys.js
@@ -1,4 +1,4 @@
-export const keys = [
+export const KEYS = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
   ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACK']
--- src/constants/wordsList.js
+++ src/constants/wordsList.js
@@ -1,4 +1,4 @@
-export const VALID_GUESSES = [
+export const WORDS_LIST = [
   'aahed',
   'aalii',
   'aargh',
