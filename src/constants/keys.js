476c22b7b2a42aa1b550325eaaa7681644c9d6d7
Dale <dale.vand@kakaoenterprise.com>
2022-02-13 11:23:44 +0900

    Refact: 매직넘버 등 constants로 분리
    기존에 utils에서 const.js로 관리하던 상수값들을 별도 디렉토리로
    분리하였습니다.

--- src/constants/keys.js
+++ src/constants/keys.js
@@ -3,7 +3,3 @@ export const keys = [
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
   ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACK']
 ];
-
-const TRY_NUM = 6;
-const WORDS_LEN = 5;
-export const ITEM_NUM = WORDS_LEN * TRY_NUM;
---
+++ src/constants/settings.js
@@ -0,0 +1,3 @@
+export const TRY_NUM = 6;
+export const WORDS_LEN = 5;
+export const ITEM_NUM = WORDS_LEN * TRY_NUM;
